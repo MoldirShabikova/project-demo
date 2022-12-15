5d67a142099afb1f06db0c361b413ead155f7211
moldir <moldirshb@gmail.com>
2022-12-15 11:11:27 -0600

    changes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
-const app = express()
+const app = express ()
 const {SERVER_PORT} = process.env
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static)
+app.use(express.static('public'))
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(_dirname, '..public/index.html'))
+//entry-point for our website
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
-app.listen(SERVER_PORT, ()=>console.log(`server running on ${SERVER_PORT}`))
+app.listen(SERVER_PORT,() => console.log(`server running on ${SERVER_PORT}`))
